3e21fb828e19f7ddafc231d3842e78c9a7c100f3
Joe Alves <joe@fullstackacademy.com>
2015-12-01 10:01:18 -0500

    Day 2: creating portfolio of projects

--- app/index.js
+++ app/index.js
@@ -1,15 +1,31 @@
 var path = require('path');
 var express = require('express');
 var morgan = require('morgan');
+var swig = require('swig');
 var app = express();
+var models = require('../models');
+var Project = models.Project;
 module.exports = app;
+
+app.engine('html', swig.renderFile);
+swig.setDefaults({ cache: false });
+app.set('view engine', 'html');
+app.set('views', path.join(__dirname, '../views'));
 
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(express.static(path.join(__dirname, '../node_modules')));
 
-var pathToIndexHTML = path.join(__dirname, '../', './views/index.html');
-
 app.get('/', function (req, res) {
-    res.sendFile(pathToIndexHTML);
+    res.render('index');
+});
+
+app.get('/portfolio', function (req, res) {
+    Project.find({}).exec()
+        .then(function (projects) {
+            res.render('portfolio', { projects: projects });
+        }).then(null, function (err) {
+            console.error(err);
+        });
+
 });
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var http = require('http');
 var server = http.createServer();
 
+require('./models');
+
 server.listen(1337, function () {
     console.log('Server is listening on port 1337!');
 });
